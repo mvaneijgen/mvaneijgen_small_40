9a38b581e3cc949224a7f078d84d3881abd65d9d
Mitchel van Eijgen <mvaneijgen@gmail.com>
2021-03-28 10:54:12 +0200

    combined code and num to numsym and add CTRL layer

--- config.h
+++ config.h
@@ -247,7 +247,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Custom code by mvaneijgen 
 //------------------------------------------------------//
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
