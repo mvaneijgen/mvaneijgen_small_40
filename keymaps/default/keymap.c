e757c4e98eb53a67289fa2f1a204af765936b2d8
Mitchel van Eijgen <mvaneijgen@gmail.com>
2021-04-08 16:26:45 +0200

    add all layers homerow

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -18,24 +18,37 @@
 #define GUI_K RGUI_T(KC_K)
 #define ALT_L LALT_T(KC_L)
 #define CTL_BSLS RCTL_T(KC_BSLS)
+
+// Using non-basic keycodes in mod-taps https://precondition.github.io/home-row-mods#using-non-basic-keycodes-in-mod-taps
+// Left-hand home row mods symbols layer
+#define CTL_COLN LCTL_T(KC_F23)
+#define ALT_LCBR LALT_T(KC_F23)
+#define GUI_LPRN LGUI_T(KC_F23)
+#define SFT_LBRC LSFT_T(KC_F23)
+
+// Right-hand home row mods symbols layer
+#define SFT_RBRC RSFT_T(KC_F22)
+#define GUI_RPRN RGUI_T(KC_F22)
+#define ALT_RCBR LALT_T(KC_F22)
+#define CTL_SCLN RCTL_T(KC_F22)
 // END 🏠️ Home row mods -------------------------------------//
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     LT(_RESET, KC_ESC), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPACE, 
 		KC_TAB, CTL_A, ALT_S, GUI_D, SFT_F, KC_G, KC_H, SFT_J, GUI_K, ALT_L, CTL_BSLS, 
-		KC_LSHIFT, KC_GRAVE, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_DOT, KC_UP, KC_DEL, 
-		KC_LCTL, KC_LALT, MO(_CTRL), LT(_CTRL, KC_ENT), KC_SPC, MO(_NUMSYM), KC_LEFT, KC_DOWN, KC_RIGHT
+		KC_NO, KC_GRAVE, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH, 
+		KC_NO, KC_NO, MO(_NUMSYM), LT(_NUMSYM, KC_ENT), KC_SPC, MO(_CTRL), KC_NO, KC_NO, KC_NO
   ),
   [_NUMSYM] = LAYOUT(
     KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, 
-		KC_CAPS, KC_LT, KC_LCBR, KC_LPRN, KC_LBRC, KC_QUOT, KC_DQT, KC_RBRC, KC_RPRN, KC_RCBR, KC_GT,
-		KC_TRNS, KC_TILD, KC_UNDS, KC_MINS, KC_SLSH, KC_COLN, KC_SCLN, KC_BSLS, KC_PLUS, KC_COMMA, KC_QUES, KC_PIPE, 
-		KC_TRNS, KC_TRNS, TG(_GAME), KC_TRNS, KC_TRNS, MO(_NUMSYM), KC_NO, KC_NO, KC_EQL
+		KC_CAPS, CTL_COLN, ALT_LCBR, GUI_LPRN, SFT_LBRC, KC_QUOT, KC_DQT, SFT_RBRC, GUI_RPRN, ALT_RCBR, CTL_SCLN,
+		KC_TRNS, KC_TILD, KC_PIPE, KC_AT, KC_MINS, KC_DLR, KC_LT, KC_GT, KC_AMPR, KC_PLUS, KC_UNDS, KC_QUES, 
+		KC_TRNS, KC_TRNS, TG(_NUMSYM), KC_TRNS, KC_TRNS, TG(_GAME), KC_NO, KC_NO, KC_EQL
   ),
   [_CTRL] = LAYOUT(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_MFFD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_BRMD, KC_BRMU, KC__VOLDOWN, KC_MPLY, KC__VOLUP, KC__MUTE, 
+		KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_BRMD, KC_BRMU, RSFT_T(KC_VOLD), RGUI_T(KC_MPLY), LALT_T(KC_VOLU), RCTL_T(KC__MUTE), 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MRWD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
@@ -52,3 +65,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, TG(_BASE), KC_TRNS, KC_TRNS, TG(_BASE), KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+   switch (keycode) {
+		 // Left-hand home row mods symbols layer
+			case CTL_COLN: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_COLN); } return false; }
+			case ALT_LCBR: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_LCBR); } return false; }
+			case GUI_LPRN: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_LPRN); } return false; }
+			case SFT_LBRC: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_LBRC); } return false; }
+			// Right-hand home row mods symbols layer
+			case SFT_RBRC: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_RBRC); } return false; }
+			case GUI_RPRN: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_RPRN); } return false; }
+			case ALT_RCBR: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_RCBR); } return false; }
+			case CTL_SCLN: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_SCLN); } return false; }
+
+			break;
+   }
+   return true;
+}
