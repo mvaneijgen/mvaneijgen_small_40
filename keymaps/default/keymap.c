9a4250d33a10fa2d746f4afa3a60451a0a18de04
Mitchel van Eijgen <mvaneijgen@gmail.com>
2021-08-07 10:00:15 +0200

    made layout inline with split version

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -32,41 +32,62 @@
 #define ALT_RCBR LALT_T(KC_F22)
 #define CTL_SCLN RCTL_T(KC_F22)
 // END 🏠️ Home row mods -------------------------------------//
+//------------------------------------------------------//
+// 💃 Tap Dance  
+//------------------------------------------------------//
+#define KC_CMDZ LGUI(KC_Z) // Custom CMD + Z
+#define KC_CMDX LGUI(KC_X) // Custom CMD + X
+#define KC_CMDC LGUI(KC_C) // Custom CMD + C
+#define KC_CMDV LGUI(KC_V) // Custom CMD + V
 
-enum custom_keycodes {
-    KC_BSPCDEL,
+enum {
+  TD_Z_CMDZ,
+  TD_X_CMDX,
+  TD_C_CMDC,
+  TD_V_CMDV,
 };
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_Z_CMDZ] = ACTION_TAP_DANCE_DOUBLE(KC_Z, KC_CMDZ),
+  [TD_X_CMDX] = ACTION_TAP_DANCE_DOUBLE(KC_X, KC_CMDX),
+  [TD_C_CMDC] = ACTION_TAP_DANCE_DOUBLE(KC_C, KC_CMDC),
+  [TD_V_CMDV] = ACTION_TAP_DANCE_DOUBLE(KC_V, KC_CMDV),
+};
+// END 💃 Tap Dance  -------------------------------------//
+// enum custom_keycodes {
+//     KC_BSPCDEL,
+// };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    LT(_RESET, KC_ESC), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPCDEL, 
+    LT(_RESET, KC_ESC), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
     KC_TAB, CTL_A, ALT_S, GUI_D, SFT_F, KC_G, KC_H, SFT_J, GUI_K, ALT_L, CTL_BSLS, 
-    KC_NO, KC_GRAVE, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, 
-    KC_NO, KC_NO, MO(_NUMSYM), LT(_NUMSYM, KC_ENT), KC_SPC, MO(_CTRL), KC_NO, KC_NO, KC_NO
+    _______, KC_GRAVE, TD(TD_Z_CMDZ), TD(TD_X_CMDX), TD(TD_C_CMDC), TD(TD_V_CMDV), KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, 
+    _______, _______, MO(_NUMSYM), LT(_NUMSYM, KC_ENT), KC_SPC, MO(_CTRL), _______, _______, _______
   ),
   [_NUMSYM] = LAYOUT(
-    KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS, 
+    _______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______, 
     KC_CAPS, CTL_COLN, ALT_LCBR, GUI_LPRN, SFT_LBRC, KC_QUOT, KC_DQT, SFT_RBRC, GUI_RPRN, ALT_RCBR, CTL_SCLN,
-    KC_TRNS, KC_TILD, KC_PIPE, KC_AT, KC_MINS, KC_DLR, KC_LT, KC_GT, KC_AMPR, KC_PLUS, KC_UNDS, KC_QUES, 
-    KC_TRNS, KC_TRNS, TG(_NUMSYM), KC_TRNS, KC_TRNS, TG(_GAME), KC_NO, KC_NO, KC_EQL
+    _______, KC_TILD, KC_AT, KC_HASH, KC_MINS, KC_DLR, KC_EQL, KC_PLUS, KC_AMPR, KC_DOT, KC_UNDS, KC_TILDE, 
+    _______, _______, TG(_NUMSYM), _______, _______, _______, _______, _______, KC_EQL
   ),
   [_CTRL] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_MFFD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_BRMD, KC_BRMU, RSFT_T(KC_VOLD), RGUI_T(KC_MPLY), LALT_T(KC_VOLU), RCTL_T(KC__MUTE), 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MRWD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    _______, _______, _______, KC_UP, _______, _______, _______, KC_MFFD, _______, _______, _______, _______, 
+    _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_BRMD, KC_BRMU, RSFT_T(KC_VOLD), RGUI_T(KC_MPLY), LALT_T(KC_VOLU), RCTL_T(KC__MUTE), 
+    _______, _______, _______, _______, _______, _______, _______, KC_MRWD, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
   [_GAME] = LAYOUT(
-    KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPACE, 
+    LT(_RESET, KC_ESC), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPACE, 
     KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_BSLS, 
     KC_LSHIFT, KC_GRAVE, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_UP, MO(_RESET), 
     KC_LCTL, KC_LALT, KC_LCMD, KC_SPC, KC_ENT, MO(_NUMSYM), KC_LEFT, KC_DOWN, KC_RIGHT
   ),
   [_RESET] = LAYOUT(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-    KC_TRNS, KC_TRNS, TG(_BASE), KC_TRNS, KC_TRNS, TG(_BASE), KC_TRNS, KC_TRNS, KC_TRNS
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET, 
+    TG(_GAME), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, _______, TG(_BASE), _______, _______, TG(_BASE), _______, _______, _______
   ),
 };
 
@@ -74,33 +95,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
    switch (keycode) {
      // Left-hand home row mods symbols layer
       case CTL_COLN: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_COLN); } return false; }
-      case ALT_LCBR: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_LCBR); } return false; }
+      case ALT_LCBR: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_LBRC); } return false; }
       case GUI_LPRN: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_LPRN); } return false; }
-      case SFT_LBRC: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_LBRC); } return false; }
+      case SFT_LBRC: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_LCBR); } return false; }
       // Right-hand home row mods symbols layer
-      case SFT_RBRC: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_RBRC); } return false; }
+      case SFT_RBRC: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_RCBR); } return false; }
       case GUI_RPRN: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_RPRN); } return false; }
-      case ALT_RCBR: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_RCBR); } return false; }
+      case ALT_RCBR: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_RBRC); } return false; }
       case CTL_SCLN: if (record->tap.count > 0) { if (record->event.pressed) { tap_code16(KC_SCLN); } return false; }
       // Custom shift keys
-      static uint8_t saved_mods = 0; // Place this outside of the switch, but inside process_record_user()
-case KC_BSPCDEL:
-    if (record->event.pressed) {
-        if (get_mods() & MOD_MASK_SHIFT) {
-            saved_mods = get_mods() & MOD_MASK_SHIFT; // Mask off anything that isn't Shift
-            del_mods(saved_mods); // Remove any Shifts present
-            register_code(KC_DEL);
-        } else {
-            saved_mods = 0; // Clear saved mods so the add_mods() below doesn't add Shifts back when it shouldn't
-            register_code(KC_BSPC);
-        }
-    } else {
-        add_mods(saved_mods);
-        unregister_code(KC_DEL);
-        unregister_code(KC_BSPC);
-    }
+//       static uint8_t saved_mods = 0; // Place this outside of the switch, but inside process_record_user()
+// case KC_BSPCDEL:
+//     if (record->event.pressed) {
+//         if (get_mods() & MOD_MASK_SHIFT) {
+//             saved_mods = get_mods() & MOD_MASK_SHIFT; // Mask off anything that isn't Shift
+//             del_mods(saved_mods); // Remove any Shifts present
+//             register_code(KC_DEL);
+//         } else {
+//             saved_mods = 0; // Clear saved mods so the add_mods() below doesn't add Shifts back when it shouldn't
+//             register_code(KC_BSPC);
+//         }
+//     } else {
+//         add_mods(saved_mods);
+//         unregister_code(KC_DEL);
+//         unregister_code(KC_BSPC);
+//     }
 
-    return false;
-   }
+//     return false;
+    }
    return true;
 }
